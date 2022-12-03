8f0af69411beb3218768108d8edc0023305e1a63
French Cat <89495053+French-Cat@users.noreply.github.com>
2022-12-03 18:06:13 +0100

    Update experiments.plugin.js

--- plugins/experiments.plugin.js
+++ plugins/experiments.plugin.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
 webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => cache=Object.values(r.c)]);
 var UserStore = cache.find(m => m?.exports?.default?.getCurrentUser).exports.default;
 var actions = UserStore._dispatcher._actionHandlers._orderedActionHandlers["CONNECTION_OPEN"];
@@ -7,5 +6,4 @@ actions.find(n => n.name === "ExperimentStore").actionHandler({
 	type: "CONNECTION_OPEN", user: {flags: user.flags |= 1}, experiments: [],
 });
 actions.find(n => n.name === "DeveloperExperimentStore").actionHandler();
-webpackChunkdiscord_app.pop(); user.flags &= ~1; "done";
-});
+webpackChunkdiscord_app.pop(); user.flags &= ~1;
